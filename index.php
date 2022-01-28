57b9c6682a3c84cbbf479b2ad58a83acf57157e4
scollo26 <giovanniscollo26@gmail.com>
2022-01-28 15:44:24 +0100

    censura con Get e con variabile

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall’utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 
-
+<?php 
+    $paragrafo = 'Questa concessionaria è autorizzata a vendere  i marchi di: Bmw, Ferrari,  Fiat, Lamborghini';
+    $lenParagrafo = strlen($paragrafo);
+    $marca_auto = $_GET['marca'];
+    $censura = str_ireplace($marca_auto, "***",$paragrafo);
+    $censura2 = str_ireplace('bmw' ,"***", $paragrafo);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +20,17 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
     <title>Document</title>
 </head>
 <body>
-ciao
-    
+<h3> <?php echo $paragrafo ?></h3>
+<p> La lunghezza del paragrafo è di: <?php echo $lenParagrafo ?> caratteri</p>
+
+<!-- php con GET -->
+
+<h3> <?php echo $censura ?> </h3>
+
+<!-- php con scelta variabile da cancellare -->
+<h3> <?php echo $censura2 ?> </h3>
+
+<p> La lunghezza del paragrafo è di: <?php echo strlen($censura); ?> caratteri </p>
 </body>
 </html>
 
